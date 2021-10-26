6a4f58cfadd9d988867fc0f2d522903b67f2d03c
Ayoub ASSAOUD <86426829+Ayoub-Vip@users.noreply.github.com>
2021-10-26 23:14:01 +0200

    Update individual.c

--- individual.c
+++ individual.c
@@ -1,2 +1,55 @@
-//bonjour greg
-hello 
+#include <stdlib.h>
+#include <stdio.h>
+#include "individual.h"
+
+//a chaque fois je modifie/change/cree le code je le mis dans le github ;)
+
+Individual *individualCreate(int length, int nbVal) {
+
+	Individual * newIndividual = malloc(sizeof(Individual));
+	
+
+}
+
+
+void individualFree(Individual *ind) {
+
+	
+}
+
+
+int individualGetLength(Individual *ind) {
+
+	
+}
+
+
+int individualGetGene(Individual *ind, int i) {
+
+	
+}
+
+
+void individualSetGene(Individual *ind, int i, int val) {
+
+	
+}
+
+
+void individualRandomInit(Individual *ind) {
+
+	
+}
+
+
+void individualRandomPermInit(Individual *ind) {
+
+
+}
+
+
+Individual *individualCopy(Individual *ind) {
+  
+
+}
+
