9bc04b807d0efcd63f379a0f114a0a023340ac1d
Ayoub ASSAOUD <86426829+Ayoub-Vip@users.noreply.github.com>
2021-10-30 16:29:34 +0200

    Update population.c

--- population.c
+++ population.c
@@ -20,19 +20,23 @@ struct Population_t {
 	int length;
 	int nbVal;
 	int size;
-        int eliteSize;
-	int **genoTypes;
+    int eliteSize;
 	float pmutation;
-        double (*fitness)(Individual *, void *);
-        void *paramFitness;
-        void (*init)(Individual *);
-        void (*mutation)(Individual *, float);
-        Individual* (*crossover)(Individual *, Individual *);
+    double (*fitness)(Individual *, void *);
+    void *paramFitness;
+    void (*init)(Individual *);
+    void (*mutation)(Individual *, float);
+	Individual** tableInd;
+    Individual* (*crossover)(Individual *, Individual *);
 
 };
+
+
+
+
 Population *populationInit(int length, int nbVal, int size,
                            double (*fitness)(Individual *, void *),
-			   void *paramFitness,
+			               void *paramFitness,
                            void (*init)(Individual *),
                            void (*mutation)(Individual *, float), float pmutation,
                            Individual* (*crossover)(Individual *, Individual *),
@@ -41,7 +45,7 @@ Population *populationInit(int length, int nbVal, int size,
 
 	Population *populationInit;
 	populationInit = mallloc(sizeof(Population));
-	populationInit->genoTypes = calloc(populationInit->size, sizeof(Individual));
+	populationInit->tableInd = calloc(populationInit->size, sizeof(Individual));
 	populationInit->length = length;
 	populationInit->nbVal = nbVal;
 	populationInit->size = size;
@@ -52,17 +56,9 @@ Population *populationInit(int length, int nbVal, int size,
 	populationInit->mutation = mutation;
 	populationInit->crossover = crossover;
 
-	for (int i = 0; i < sizeof; ++i)
-	{
-		populationInit->genoTypes[i] = init();
-	}
-
-
-
-
-
-
-
+	for (int i = 0; i < populationInit->size; ++i)
+		if(populationInit->tableInd[i] = individualCreate(length, nbVal))
+			init(populationInit->tableInd[i]);	
 
 
 
@@ -71,33 +67,69 @@ Population *populationInit(int length, int nbVal, int size,
 
 
 double populationGetMaxFitness(Population *pop) {
+	double max;
+	for (int i = 0; i < pop->size-1; ++i)
+	{
+		double a =pop->fitness(pop->tableInd[i], pop->paramFitness);
+		double b =pop->fitness(pop->tableInd[i+1], pop->paramFitness);
 
+		max = (a > b) ? a : b;
+	}
 
+	return max;
 }
 
 
 
 double populationGetMinFitness(Population *pop) {
+	double min;
 
+	for (int i = 0; i < pop->size-1; ++i)
+	{
+		double a =pop->fitness(pop->tableInd[i], pop->paramFitness);
+		double b =pop->fitness(pop->tableInd[i+1], pop->paramFitness);
+
+		min = (a < b) ? a : b;
+	}
+
+	return min;
 
 }
 
 
 
 double populationGetAvgFitness(Population *pop) {
+	double sum = 0;
 
+	for (int i = 0; i < pop->size; ++i)
+	{
+		sum += pop->fitness(pop->tableInd[i], pop->paramFitness);
+	}
+
+	return (sum/(double) pop->size);
 
 }
 
 
 Individual *populationGetBestIndividual(Population *pop) {
+	double maxFitness = populationGetAvgFitness(pop)
 
+	for (int i = 0; i < pop->size; ++i)
+	{
+		if(pop->fitness(pop->tableInd[i], pop->paramFitness) == maxFitness)
+			return pop->tableInd[i];
+	}
 
 }
 
 
 void populationFree(Population *pop) {
 
+	for (int i = 0; i < pop->size; ++i)
+		free(pop->tableInd[i]);
+	
+	free(pop->tableInd);
+	free(pop);
 
 }
 
