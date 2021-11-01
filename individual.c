8ff7786639fd9927664647eabd89e11ffef67115
GregFreddy <93208192+GregFreddy@users.noreply.github.com>
2021-11-01 11:49:45 +0100

    Update individual.c

--- individual.c
+++ individual.c
@@ -53,21 +53,45 @@ void individualSetGene(Individual *ind, int i, int val) {
 
 void individualRandomInit(Individual *ind) {
 
-	for (int i = 0; i < ind->length; ++i)
-		ind->genoType[i] = rand()%ind->nbVal;
+	genoType* genotype = ind->genoType;
+
+	for (int i = 0; i < ind->length; i++)
+	{
+		genotype[i] = rand()%2;
+	}
+	
+	ind->genoType = genotype;		
 }
 
 
 void individualRandomPermInit(Individual *ind) {
-	int tmp_index1 = rand()%ind->length;
-	int tmp_index2 = rand()%ind->length;
-
-	int tmp_val1    = ind->genoType[tmp_index1];
-	ind->genoType[tmp_index1]  = ind->genoType[tmp_index2];
-	ind->genoType[tmp_index2]  = tmp_val1;
-
-
-	ind->genoType[rand()%ind->length] = ind->genoType[rand()%ind->length];
+	
+	genoType* genotype = ind->genotype;
+	int length = ind->length;
+	int nbVal = ind->nbVal;
+	int i;
+	
+	for( i = 0; i<length; i++)
+	{
+		genotype[i] = i;
+	}
+	
+	for( i = 0; i<length, i++)
+	{
+		int p = rand()%nbVal;
+		int q = rand()%nbVal;
+		
+		if( p == q)
+			continue;
+		else
+		{
+			int a = genotype[p];
+			genotype[p] = genotype[q];
+			genotype[q] = a;
+		}
+	}
+	
+	ind->genoType = genotype;	
 
 }
 
@@ -106,16 +130,84 @@ void individualPrint(FILE *fp, Individual *ind) {
 
 void individualSeqMutation(Individual *ind, float pm) {
 	
+		genoType* genotype = ind->genoType;
+	int length = ind->length;
+	
+	for(;;;)
+	{
+		int p = rand()%length;
+		int q = rand()%length;
+		
+		if( p!=q)
+			break;
+	}
+	
+	int a = genotype[p];
+	genotype[p] = genotype[q];
+	genotype[q] = a;
+	
 }
 
 
 Individual* individualSeqCrossOver(Individual *parent1, Individual *parent2) {
+	
+	genoType* genotype1 = parent1->genoType;
+	genoType* genotype2 = parent2->genoType;
+	int length = parent1->length;
+	
+	Individual* enfant = malloc(sizeof(Individual));
+	genoType* geno_enfant = enfant->genoType;
+	
+	int i;
+	int p = rand()%length;
+	
+	for( i = 0; i<p ; i++)
+		geno_enfant[i] = genotype1[i];
+	
+	for( i = p; i<length; i++)
+		geno_enfant[i] = genotype2[i];
+	
+	enfant->genoType = geno_enfant;
+	
+	return enfant;	
 
 
 }
 
 
 void individualPermMutation(Individual *ind, float pm) {
+	
+	genoType* genotype = ind->genoType;
+	int length = ind->length;
+	
+	for(;;;)
+	{
+		int p1 = rand()%length;
+		int p2 = rand()%length;
+		
+		if(p1!=p2)
+			break;
+	}
+	
+	if( p1>p2)
+	{
+		int a = p1;
+		p1 = p2;
+		p2 = a;
+	}
+	
+	for(;p1>=p2;)
+	{
+		int a = genotype[p1];
+		genotype[p1] = genotype[p2];
+		genotype[p2] = a;
+		
+		p1++;
+		p2--;
+	}
+	
+	ind->genoType = genotype;
+	
 
 
 }
